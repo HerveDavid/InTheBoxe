475adaa542c0bae79c4c78637a1ecbe6fc50680f
David HERVE <david.herve99@gmail.com>
2019-11-09 15:43:29 +0100

    accueil html

---
+++ view/accueil.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="src/style/accueil/style.css">
+    <meta charset="utf-8">
+    <title>InThBoxe</title>
+  </head>
+  <body>
+    <header>
+      <h1 class="logo"><img src="src/img/logo.png" alt="logo-InTheBoxe"></h1>
+      <nav class="topNavigation">
+        <ul>
+          <li><a href="#Accueil">Accueil</a></li>
+          <li><a href="#Club">Club</a></li>
+          <li><a href="#Entrainement">Entrainement</a></li>
+          <li><a href="#Competition">Competition</a></li>
+          <li><a href="#Contact">Contact</a></li>
+        </ul>
+        <button type="button" name="connexion">Connexion</button>
+      </nav>
+    </header>
+    <div id="Content">
+      <h2>Boxing Club Evian-Les-Bains</h2>
+      <h1>Explorez vos limites</h1>
+      <p>Inscrivez vous pour avoir accès à des cours gratuits et aux tournois de boxe en ligne</p>
+      <a href="#Inscription">Inscription</a>
+    </div>
+    <footer>
+      <h1>Accès aux cours et informations</h1>
+      <h1>Top 3 cours</h1>
+      <a href="#Cours1"><img src="src/img/accueil/cours1.png" alt="cours-1"></a>
+      <a href="#Cours2"><img src="src/img/accueil/cours2.png" alt="cours-2"></a>
+      <a href="#Cours3"><img src="src/img/accueil/cours2.png" alt="cours-3"></a>
+    </footer>
+  </body>
+</html>
