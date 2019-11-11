91653e802139610d6a24184492c40505556eb760
David HERVE <david.herve99@gmail.com>
2019-11-11 16:41:27 +0100

    css link

--- view/club.view.php
+++ view/club.view.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/css?family=Palanquin|Rubik&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../view/src/style/club.css">
     <title>InTheBoxe</title>
   </head>
   <body>
