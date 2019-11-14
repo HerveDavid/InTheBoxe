b53a0c5e1633f89046b0dcb20df028fef2fb962b
BENMANSOUR Amine <benmansa@hostname.iut2.upmf-grenoble.fr>
2019-11-14 11:30:27 +0100

    bd

--- model/data/db/create.sql
+++ model/data/db/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE adherent(
-  license INTEGER PRIMARY KEY,
+  licence INTEGER PRIMARY KEY,
   nom VARCHAR(25),
   prenom VARCHAR(25),
   datenaiss DATE,
@@ -30,7 +30,7 @@ CREATE TABLE cours(
 CREATE TABLE prof(
   numprof INTEGER PRIMARY KEY,
   nomprof VARCHAR(25),
-  prenomptof VARCHAR(25)
+  prenomprof VARCHAR(25)
 );
 
 CREATE TABLE activite(
@@ -39,7 +39,7 @@ CREATE TABLE activite(
 );
 
 CREATE TABLE boxeurExt(
-  license INTEGER PRIMARY KEY,
+  licence INTEGER PRIMARY KEY,
   nom VARCHAR(25),
   datenaiss DATE,
   categorie VARCHAR(25),
@@ -57,4 +57,59 @@ CREATE TABLE club(
 
 CREATE TABLE exercices(
   nom VARCHAR(25) PRIMARY KEY
+);
+
+CREATE TABLE participe(
+  numlicence INTEGER,
+  type VARCHAR(20),
+  jour VARCHAR(20),
+  horaire VARCHAR(20),
+  PRIMARY KEY(numlicence,type,jour,horaire),
+  FOREIGN KEY(numlicence) REFERENCES adherent(licence),
+  FOREIGN KEY(type,jour,horaire) REFERENCES cours(type,jour,horaire)
+);
+
+CREATE TABLE coursDeLaSemaine(
+  type VARCHAR(20),
+  horaire VARCHAR(20),
+  datecours DATE,
+  numprof INTEGER,
+  nbinscrits INTEGER,
+  PRIMARY KEY(horaire,datecours),
+  FOREIGN KEY(type,horaire) REFERENCES cours(type,horaire),
+  FOREIGN KEY(numprof) REFERENCES prof(numprof)
+);
+
+CREATE TABLE entrainementPerso(
+  numlicence INTEGER,
+  numentrainement INTEGER,
+  nomexercice VARCHAR(25),
+  nbrep INTEGER,
+  PRIMARY KEY(numlicence,numentrainement,nomexercice),
+  FOREIGN KEY(numlicence) REFERENCES adherent(licence),
+  FOREIGN KEY(nomexercice) REFERENCES exercices(nom)
+);
+
+CREATE TABLE combatOfficiel(
+  licenceint INTEGER,
+  licenceext INTEGER,
+  dateprevue DATE,
+  horaire VARCHAR,
+  PRIMARY KEY(licenceint,licenceext,dateprevue),
+  FOREIGN KEY(licenceint) REFERENCES adherent(licence),
+  FOREIGN KEY(licenceext) REFERENCES boxeurExt(licence)
+);
+
+CREATE TABLE matchPotentiel(
+ boxeurint VARCHAR(25),
+ licenceint INTEGER,
+ boxeurext VARCHAR(25),
+ licenceext INTEGER,
+ clubext VARCHAR(25),
+ categorie VARCHAR(20),
+ datecombat DATE,
+ lieu VARCHAR(25),
+ PRIMARY KEY(licenceint,licenceext,datecombat),
+ FOREIGN KEY(boxeurint,licenceint,categorie) REFERENCES adherent(prenom,licence,categorie),
+ FOREIGN KEY(boxeurext,licenceext) REFERENCES boxeurExt
 );
