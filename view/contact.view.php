b5ae50c5df1ea2b7e060037f00b249853a31d8ca
Herve David <david@localhost.localdomain>
2020-01-11 23:05:05 +0100

    ok

--- view/contact.view.php
+++ view/contact.view.php
@@ -59,91 +59,92 @@
       </nav>
     </header>
 
-    <div class="boxe-main-full">
-      <article class="boxe-article">
-          <div class="boxe-map">
-            <h5>CONTACT</h5>
-            <h1>Restons en contact</h1>
-            <p>
-                <img src="../view/src/img/contact/gantsBoxe.png" alt="gantsBoxe">
-            </p>
-
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2751.449025643487!2d6.586565315525784!3d46.40015197912312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478c3cab6d4c055f%3A0x4fa7470265a3f22a!2sImpasse%20Dr%20Dumur%2C%2074500%20%C3%89vian-les-Bains!5e0!3m2!1sfr!2sfr!4v1575628999904!5m2!1sfr!2sfr"
-            width="1300"
-            height="300"
-            frameborder="0"
-            align="middle"
-            style="border:0;"
-            allowfullscreen="true">
-            </iframe>
-
-          </div>
-      </article>
-    </div>
-    <div class="boxe-main-gauche">
+    <div class="boxe-main-full d-flex flex-wrap justify-content-center">
+      <div class="d-flex flex-column bd-highlight mb-3 ">
+        <h5 class="text-center">CONTACT</h5>
+        <h1>Restons en contact</h1>
         <p>
-            <img src="../view/src/img/contact/mail.png" alt="enveloppe">
+            <img src="../view/src/img/contact/gantsBoxe.png" alt="gantsBoxe">
         </p>
-        <p id="question">
-            Pour toutes informations complémentaires n'hésitez pas à nous contacter
+      </div>
+
+
+            <div class="embed-responsive embed-responsive-16by9">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2751.449025643487!2d6.586565315525784!3d46.40015197912312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478c3cab6d4c055f%3A0x4fa7470265a3f22a!2sImpasse%20Dr%20Dumur%2C%2074500%20%C3%89vian-les-Bains!5e0!3m2!1sfr!2sfr!4v1575628999904!5m2!1sfr!2sfr"
+              class="embed-responsive-item"
+              allowfullscreen
+              >
+            </iframe>
+            </div>
+            <h1>kjmlk</h1>
+    </div>
+    <!-- <div class="d-flex flex-row">
+      <div class="">
+          <p>
+              <img src="../view/src/img/contact/mail.png" alt="enveloppe">
+          </p>
+          <p id="question">
+              Pour toutes informations complémentaires n'hésitez pas à nous contacter
+          </p>
+
+          <form action="../controle/contact.ctrl.php" method="post" class="formulaire-email">
+            <input type="email" name="email" size="40" required placeholder="E-mail" >
+            <br>
+            <input type="text" name="nom"  size="40" required  placeholder="Nom" >
+            <br>
+            <input type="text" name="sujet" size="40"  required placeholder="Sujet" >
+            <br>
+            <textarea name="message" required placeholder="Entrez votre message" ></textarea>
+            <br>
+            <input type="submit" class="bouton" value="ENVOYER">
+          </form>
+          <?php     global $confirmation; ?>
+          <p style='color:green'> <?= $confirmation ?></p>
+      </div>
+      <div class="">
+        <p id="place">
+            <img src="../view/src/img/contact/place.png" alt="place">
+        </p>
+        <h5>
+          Boxing Club Evian
+        </h5>
+        <br>
+        <p>
+          Impasse du Docteur Dumur
+          <br>
+          74500 Évian-les-Bains
+          <br>
+          06 58 87 23 90
+          <br>
+          boxingclubevian@gmail.com
+          <br>
+          Parking à proximité du club
+        </p>
+        <br>
+        <p id="titreHoraires">
+          Horaires d'ouvertures
+        </p>
+        <br>
+        <p id="horaires">
+          Lundi ……………….…......................…. 17h > 20h
+          <br>
+          Mardi ………………...............……. 18h30 > 20h
+          <br>
+          Mercredi ………….......…….….....…. 18h > 20h
+          <br>
+          Jeudi   ..........................9h > 10h30 | 18h > 20h
+          <br>
+          Vendredi ……………..…............……. 17h > 20h
+          <br>
+          Samedi ……......……….....….……. 9h > 10h30
+          <br>
+          Dimanche …................…………….……. FERMÉ
         </p>
 
-        <form action="../controle/contact.ctrl.php" method="post" class="formulaire-email">
-          <input type="email" name="email" size="40" required placeholder="E-mail" >
-          <br>
-          <input type="text" name="nom"  size="40" required  placeholder="Nom" >
-          <br>
-          <input type="text" name="sujet" size="40"  required placeholder="Sujet" >
-          <br>
-          <textarea name="message" required placeholder="Entrez votre message" ></textarea>
-          <br>
-          <input type="submit" class="bouton" value="ENVOYER">
-        </form>
-        <?php     global $confirmation; ?>
-        <p style='color:green'> <?= $confirmation ?></p>
+      </div>
     </div>
 
-    <div class="boxe-main-droite">
-      <p id="place">
-          <img src="../view/src/img/contact/place.png" alt="place">
-      </p>
-      <h5>
-        Boxing Club Evian
-      </h5>
-      <br>
-      <p>
-        Impasse du Docteur Dumur
-        <br>
-        74500 Évian-les-Bains
-        <br>
-        06 58 87 23 90
-        <br>
-        boxingclubevian@gmail.com
-        <br>
-        Parking à proximité du club
-      </p>
-      <br>
-      <p id="titreHoraires">
-        Horaires d'ouvertures
-      </p>
-      <br>
-      <p id="horaires">
-        Lundi ……………….…......................…. 17h > 20h
-        <br>
-        Mardi ………………...............……. 18h30 > 20h
-        <br>
-        Mercredi ………….......…….….....…. 18h > 20h
-        <br>
-        Jeudi   ..........................9h > 10h30 | 18h > 20h
-        <br>
-        Vendredi ……………..…............……. 17h > 20h
-        <br>
-        Samedi ……......……….....….……. 9h > 10h30
-        <br>
-        Dimanche …................…………….……. FERMÉ
-      </p>
 
-    </div>
 
     <div id="fb-root">
       <script async defer crossorigin="anonymous"
@@ -161,7 +162,7 @@
           <a href="https://www.facebook.com/Boxing-Club-Evian-1779416988982586/">Boxing Club Evian</a>
         </blockquote>
       </div>
-    </div>
+    </div> -->
 
   </body>
 </html>
